43780f29d9dd0051c7dd2d22acbf44fe57ff5e7f
Jiangshuuu <globelex65@gmail.com>
2024-10-19 23:00:47 +0800

    regiter form action

--- schemas/index.ts
+++ schemas/index.ts
@@ -12,3 +12,17 @@ export const LoginSchema = z.object({
     message: "Password is required",
   }),
 });
+
+export const RegisterSchema = LoginSchema.extend({
+  email: z
+    .string()
+    .email({
+      message: "Email is required",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long",
+  }),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
