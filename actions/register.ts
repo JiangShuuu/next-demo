38d03e5ecfa682d6752cbcba58f631c22d47f3c4
Jiangshuuu <globelex65@gmail.com>
2024-10-31 11:16:10 +0800

    add register fn

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -11,7 +14,33 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     };
   }
 
+  const { name, email, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token to email
+
   return {
-    success: "Account created successfully",
+    success: "User created successfully",
   };
 };
