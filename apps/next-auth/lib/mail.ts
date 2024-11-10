27984b47e33d7f6acc8e8cbc0a35d5dd6b85f890
Jiangshuuu <globelex65@gmail.com>
2024-11-10 11:14:30 +0800

    add nodemailer

--- apps/next-auth/lib/mail.ts
+++ apps/next-auth/lib/mail.ts
@@ -1,9 +1,15 @@
-import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-export const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
-  await resend.emails.send({
+  await transporter.sendMail({
     from: "john@john.dev",
     to: email,
     subject: "2FA Code",
@@ -14,7 +20,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-password?token=${token}`;
 
-  await resend.emails.send({
+  await transporter.sendMail({
     from: "john@john.dev",
     to: email,
     subject: "Reset your password",
@@ -25,7 +31,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification?token=${token}`;
 
-  await resend.emails.send({
+  await transporter.sendMail({
     from: "john@john.dev",
     to: email,
     subject: "Confirm your email",
