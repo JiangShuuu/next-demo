88f5795c22a5a23ba66c7e3239c91d6178b3bc9b
Jiangshuuu <globelex65@gmail.com>
2024-11-14 21:08:13 +0800

    add test loading

--- apps/next-auth/app/(protected)/server/page.tsx
+++ apps/next-auth/app/(protected)/server/page.tsx
@@ -1,8 +1,10 @@
 import { UserInfo } from "@/components/user-info";
 import { currentUser } from "@/lib/auth";
+import { sleep } from "@/lib/promise";
 
 export default async function ServerPage() {
   const user = await currentUser();
+  await sleep(3000);
 
   return <UserInfo user={user} label="Server component" />;
 }
