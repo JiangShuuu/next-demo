310d04ed62b7e82bcc37599a4af36bf0c694f745
Jiangshuuu <globelex65@gmail.com>
2024-11-20 11:59:17 +0800

    add infinite-query

---
+++ apps/react-query-graphql/app/Infinite-query/page.tsx
@@ -0,0 +1,54 @@
+// https://dev.to/wassim93/mastering-infinite-scrolling-with-react-query-v5-1f5i
+"use client";
+
+import { useInfiniteQuery } from "react-query";
+import { fetchData } from "./services/api";
+import { useCallback, useMemo, useRef } from "react";
+
+export default function InfiniteQueryPage() {
+  const observer = useRef<IntersectionObserver>();
+
+  const { data, error, fetchNextPage, hasNextPage, isFetching, isLoading } =
+    useInfiniteQuery({
+      queryKey: ["todos"],
+      queryFn: ({ pageParam }) => fetchData(pageParam),
+      initialPageParam: 0,
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.length > 0 ? allPages.length + 1 : undefined;
+      },
+    });
+  const flatData = useMemo(() => data?.pages.flat() ?? [], [data]);
+
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isLoading) return;
+
+      if (observer.current) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        console.log(entries);
+        if (entries[0].isIntersecting && hasNextPage && !isFetching) {
+          fetchNextPage();
+        }
+      });
+
+      if (node) observer.current.observe(node);
+    },
+    [fetchNextPage, hasNextPage, isFetching, isLoading]
+  );
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (error) return <h1>Error on fetch data...</h1>;
+
+  return (
+    <div>
+      {flatData?.map((item) => (
+        <div key={item.id} ref={lastElementRef}>
+          <p>{item.title}</p>
+        </div>
+      ))}
+
+      {isFetching && <div>Fetching more data...</div>}
+    </div>
+  );
+}
